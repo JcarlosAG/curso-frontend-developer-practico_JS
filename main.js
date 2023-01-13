fa04379b871a5dfa83c68d89ea243d254e09ac75
Juan Carlos Aguilar González <jcarlosaglez@gmail.com>
2023-01-12 22:20:57 -0600

    fusion de menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbar_email = document.querySelector('.navbar-email');
 const menuEmail = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobMenu = document.querySelector('.mobile-menu');
 
 navbar_email.addEventListener('click', toggleMenu);
+burgerMenu.addEventListener('click', toggleMblmenu);
 
 function toggleMenu() {
     menuEmail.classList.toggle('inactive');
+}
+
+function toggleMblmenu() {
+    mobMenu.classList.toggle('inactive');
 }
